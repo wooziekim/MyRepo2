07c56f272bec44a340ded5006c8e3b235f4f0fa3
kimtaewoo <wooziekim@gmail.com>
2019-12-30 12:49:58 +0900

    Third commit

--- Git03_Java/src/test/main/MainClass01.java
+++ Git03_Java/src/test/main/MainClass01.java
@@ -4,5 +4,7 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("_");
 		System.out.println("__");
+		System.out.println("___");
+		System.out.println("____");
 	}
 }
